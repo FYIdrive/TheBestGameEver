282f7d428435cc66c3c7c1baed0f0212dcc73bce
Egor Cherepanov <dekma_777@mail.ru>
2024-02-16 18:14:50 +0600

    Моб преследует игрока

--- Assets/scripts/EnemyAI.cs
+++ Assets/scripts/EnemyAI.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
-   public List<Transform> patrolPoints;
+    public List<Transform> patrolPoints;
     public PlayerController player;
     public float viewAngle;
 
 
     private NavMeshAgent _navMeshAgent;
     private bool _isPlayerNoticed;
-    
+
     private void Start()
     {
         InitComponentLinks();
@@ -21,18 +21,19 @@ public class EnemyAI : MonoBehaviour
 
     private void InitComponentLinks()
     {
-        
+
         _navMeshAgent = GetComponent<NavMeshAgent>();
     }
     private void Update()
     {
         NoticePlayerUpdate();
+        ChaseUpdate();
         PatrolUpdate();
     }
     private void NoticePlayerUpdate()
     {
-      var direction = player.transform.position - transform.position;
-     _isPlayerNoticed = false;
+        var direction = player.transform.position - transform.position;
+        _isPlayerNoticed = false;
         if (Vector3.Angle(transform.forward, direction) < viewAngle)
         {
             RaycastHit hit;
@@ -43,18 +44,28 @@ public class EnemyAI : MonoBehaviour
                     _isPlayerNoticed = true;
                 }
             }
-        }    
+        }
     }
 
-    private void PatrolUpdate() 
-    { 
-    if (_navMeshAgent.remainingDistance == 0 )
-        {
+    private void PatrolUpdate()
+    {
+      if (!_isPlayerNoticed) 
+      {
+         if (_navMeshAgent.remainingDistance == 0)
+         {
             PickNewPatrolPoint();
-        }
+         }
+      }
     }
     private void PickNewPatrolPoint()
     {
         _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+    private void ChaseUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = player.transform.position;
+        }
     }
 }
