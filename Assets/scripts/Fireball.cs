38576cecd027addf1cabd0886a57c03960529cde
Egor Cherepanov <dekma_777@mail.ru>
2024-02-22 22:13:45 +0600

    здоровье моба

--- Assets/scripts/Fireball.cs
+++ Assets/scripts/Fireball.cs
@@ -6,6 +6,7 @@ public class Fireball : MonoBehaviour
 {
     public float speed;
     public float lifetime;
+    public float damage = 10;
 
     void FixedUpdate()
     {
@@ -19,9 +20,19 @@ public class Fireball : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        DamageEnemy(collision);
         DestroyFireball();
     }
 
+    private void DamageEnemy (Collision collision)
+    {
+        var enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.DealDamage(damage);
+        }
+
+    }
     private void DestroyFireball()
     {
         Destroy(gameObject);
