4c314c20560548baa07faff88769885de3534eb3
Egor Cherepanov <dekma_777@mail.ru>
2024-02-27 23:15:38 +0600

    Хелсбар

--- Assets/scripts/EnemyAI.cs
+++ Assets/scripts/EnemyAI.cs
@@ -8,10 +8,11 @@ public class EnemyAI : MonoBehaviour
     public List<Transform> patrolPoints;
     public PlayerController player;
     public float viewAngle;
-
+    public float damage = 30;
 
     private NavMeshAgent _navMeshAgent;
     private bool _isPlayerNoticed;
+    private PlayerHealth _playerHealth;
 
     private void Start()
     {
@@ -23,13 +24,27 @@ public class EnemyAI : MonoBehaviour
     {
 
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        _playerHealth = player.GetComponent<PlayerHealth>();
     }
     private void Update()
     {
         NoticePlayerUpdate();
         ChaseUpdate();
+        AttackUpdate();
         PatrolUpdate();
     }
+
+    private void AttackUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+            {
+                _playerHealth.DealDamage(damage * Time.deltaTime);
+            }
+        }
+    }
+
     private void NoticePlayerUpdate()
     {
         var direction = player.transform.position - transform.position;
@@ -51,7 +66,7 @@ public class EnemyAI : MonoBehaviour
     {
       if (!_isPlayerNoticed) 
       {
-         if (_navMeshAgent.remainingDistance == 0)
+         if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
          {
             PickNewPatrolPoint();
          }
